cfdab74f3ce3f54f9b84bbdb0d0ab342a57fdd48
berkelpam <paul.tools@icloud.com>
2017-07-19 15:20:09 +0200

    version 1 vehicles

---
+++ Vehicles/src/Volkswagen.java
@@ -0,0 +1,11 @@
+public class Volkswagen {
+   public static void main(String args[]) {
+     Car b1 = new Car();
+     // the subclass can inherit 'color' member of the superclass
+     b1.color = "blue"; 
+     b1.speed=200;
+     b1.size=100;
+     b1.CC = 1000;
+     b1.gears = 5;
+  }
+}
